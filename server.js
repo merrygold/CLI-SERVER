322cf4826c614318011265d0be1f3a1e61a1ca88
merrygold <merrygold643@gmail.com>
2023-09-18 02:26:22 +0500

    Cors

--- server.js
+++ server.js
@@ -7,40 +7,6 @@ const {S3Client , PutObjectCommand ,GetObjectCommand ,DeleteObjectCommand } = re
 const dotenv = require('dotenv')
 
 
-
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
-
-
-
-
-
-
-
-
-
 // Enable CORS for specific origins (in this case, 'https://cli-repl.vercel.app')
 app.use(
   cors({
