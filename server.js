bdb1b8fe621e57870e4bbbe543762f9c94be4e5f
merrygold <merrygold643@gmail.com>
2023-09-18 01:10:46 +0500

    BACKEND FINAL

--- server.js
+++ server.js
@@ -1,83 +1,124 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-const { v4: uuidv4 } = require('uuid');
-const Datastore = require('nedb');
-// Allow requests from any origin (you can change this if needed)
+const {S3Client , PutObjectCommand ,GetObjectCommand ,DeleteObjectCommand } = require('@aws-sdk/client-s3')
+const dotenv = require('dotenv')
 
-const db = new Datastore({ filename: 'csv-file.db', autoload: true });
 
 const corsOptions = {
   origin: 'https://cli-repl.vercel.app/',
 };
 
 app.use(cors(corsOptions));
+dotenv.config();
+
+// * Set up Multer with the storage
+const storage = multer.memoryStorage();
+const upload = multer({storage});
 
 
-// Define the destination and filename for the uploaded files
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, 'draw-chart'));
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${file.originalname}`);
+// *! CloudFlare Credentials
+const S3 = new S3Client({
+  region: 'auto',
+  endpoint: process.env.ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
   },
 });
 
-// Set up Multer with the storage
-const upload = multer({ storage });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  // Check if a file was uploaded successfully
-  if (req.file) {
-    const { originalname, mimetype } = req.file;
-    // Check if the uploaded file has the CSV mimetype
-    if (mimetype === 'text/csv') {
-      // Generate a unique filename using UUID
-      const uniqueFilename = uuidv4() + path.extname(originalname);
-      
-      // Move the uploaded file to the storage directory
-      const storagePath = path.join(__dirname, 'draw-chart', uniqueFilename);
-      fs.renameSync(req.file.path, storagePath);
+//* For Uploading the File on Cloudflare Storage
 
-      // Create metadata for the uploaded file
-      const metadata = {
-        filename: uniqueFilename,
-        originalname: originalname,
-        filepath: storagePath,
-        uploadDate: new Date().toISOString(),
-      };
+app.post('/upload', upload.single('file'), async (req, res) => {
 
-      // Insert the metadata into NeDB
-      db.insert(metadata, (err, newDoc) => {
-        if (err) {
-          // Handle error
-          res.status(500).json({ message: 'Internal server error.' });
-        } else {
-          res.status(200).json({ message: `${originalname} has been uploaded successfully.` });
-        }
-      });
-    } else {
-      // If the mimetype is not CSV, delete the uploaded file and return an error
-      fs.unlinkSync(req.file.path); // Delete the temporary file
-      res.status(400).json({ message: 'Only CSV files are allowed.' });
-    }
-  } else {
-    res.status(400).json({ message: 'No file uploaded.' });
+
+  await S3.send(
+    new PutObjectCommand({
+      Body: req.file.buffer,
+      Bucket: 'cli-storage',
+      Key: req.file.originalname,
+      ContentType: req.file.mimetype,
+    })
+  );
+  res.send('File Upload');
+});
+
+
+
+//* For Accessing the file with {filename} param from the Cloudflare Storage
+
+app.get('/draw-chart/:filename', async (req, res) => {
+  const { filename } = req.params; // Get the filename from the route parameters
+
+  console.log(filename)
+  try {
+    const data = await S3.send(
+      new GetObjectCommand({
+        Bucket: 'cli-storage', // Replace with your S3 bucket name
+        Key: filename, // Use the filename from the route params as the key
+      })
+    );
+
+    console.log(typeof(data))
+  
+    
+    // Set the appropriate response headers based on the file's metadata
+    res.set('Content-Type', 'text/csv');
+    res.set('Content-Disposition', `attachment; filename="${filename}"`);
+
+    // Send the file data as the response
+  
+    data.Body.pipe(res);
+  } catch (error) {
+    console.error('Error retrieving file from S3:', error);
+
+   
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+});
+
+
+
+//* For Delete file with {filename} param on the Cloudflare Storage
+
+app.delete('/delete-file/:filename', async (req, res) => {
+  const { filename } = req.params; // Get the filename from the route parameters
+
+  try {
+    const deleteParams = {
+      Bucket: 'cli-storage', // Replace with your S3 bucket name
+      Key: filename, // Use the filename from the route params as the key
+    };
+
+    await S3.send(new DeleteObjectCommand(deleteParams));
+    res.status(200).json({ message: `File "${filename}" deleted successfully.` });
+
+  
+  } catch (error) {
+    console.error('Error deleting file from S3:', error);
+    res.status(500).json({ message: 'Internal server error.' });
   }
 });
 
 
 
 
+//* For Local Storage
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.join(__dirname, 'draw-chart'));
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, `${file.originalname}`);
+//   },
+// });
 
 
-// Route for handling file uploads with format validation
-// app.post('/upload-simple', upload.single('file'), (req, res) => {
+//*? Route for handling file uploads with format validation using (local-File-Syestem)
+// app.post('/upload', upload.single('file'), (req, res) => {
 //   // Check if a file was uploaded successfully
 //   if (req.file) {
 //     const { originalname, mimetype } = req.file;
@@ -96,67 +137,73 @@ app.post('/upload', upload.single('file'), (req, res) => {
 // });
 
 
-// Serve uploaded files
-app.get('/draw-chart/:filename', (req, res) => {
-  const { filename } = req.params;
-  const filePath = path.join(__dirname, 'draw-chart', filename);
 
-  // Check if the file exists
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      // File does not exist, return a 404 response
-      return res.status(404).json({ message: 'File not found.' });
-    }
 
-    // Read the file content
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        // Handle any error that occurred while reading the file
-        return res.status(500).json({ message: 'Internal server error.' });
-      }
 
-      // Set the response headers to indicate the file type (e.g., CSV)
-      res.setHeader('Content-Type', 'text/csv');
-      res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+// * Serve uploaded files using (local-File-Syestem)
+// app.get('/draw-chart/:filename', (req, res) => {
+//   const { filename } = req.params;
+//   const filePath = path.join(__dirname, 'draw-chart', filename);
 
-      // Send the file content in the response
-      res.send(data);
-    });
-  });
-});
+//   // Check if the file exists
+//   fs.access(filePath, fs.constants.F_OK, (err) => {
+//     if (err) {
+//       // File does not exist, return a 404 response
+//       return res.status(404).json({ message: 'File not found.' });
+//     }
 
-app.delete('/delete-file/:filename', (req, res) => {
-  const { filename } = req.params;
-  const filePath = path.join(__dirname, 'draw-chart', filename);
+//     // Read the file content
+//     fs.readFile(filePath, (err, data) => {
+//       if (err) {
+//         // Handle any error that occurred while reading the file
+//         return res.status(500).json({ message: 'Internal server error.' });
+//       }
 
-  // Check if the file exists
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      // File does not exist, return a 404 response
-      return res.status(404).json({ message: 'File not found.' });
-    }
+//       // Set the response headers to indicate the file type (e.g., CSV)
+//       res.setHeader('Content-Type', 'text/csv');
+//       res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
 
-    // Delete the file
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        // Handle any error that occurred while deleting the file
-        return res.status(500).json({ message: 'Internal server error.' });
-      }
+//       // Send the file content in the response
+//       res.send(data);
+//     });
+//   });
+// });
 
-      // File deleted successfully
-      res.status(200).json({ message: 'File deleted successfully.' });
-    });
-  });
-});
 
+// * Delete the file requires a {filename} using (local-File-Syestem)
+// app.delete('/delete-file/:filename', (req, res) => {
+//   const { filename } = req.params;
+//   const filePath = path.join(__dirname, 'draw-chart', filename);
+
+//   // Check if the file exists
+//   fs.access(filePath, fs.constants.F_OK, (err) => {
+//     if (err) {
+//       // File does not exist, return a 404 response
+//       return res.status(404).json({ message: 'File not found.' });
+//     }
+
+//     // Delete the file
+//     fs.unlink(filePath, (err) => {
+//       if (err) {
+//         // Handle any error that occurred while deleting the file
+//         return res.status(500).json({ message: 'Internal server error.' });
+//       }
+
+//       // File deleted successfully
+//       res.status(200).json({ message: 'File deleted successfully.' });
+//     });
+//   });
+// });
+
+
+
+// * Basic Server Check
 app.get('/', (req, res) => {
   res.send('CLI backend Server is Ready.....');
 });
 
-app.get('/get', (req, res) => {
-  res.send('CLI backend Server is Ready.....');
-});
 
+// * Basic Server Port Check on Console
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
